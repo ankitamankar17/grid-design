7f1b05061005d541163f1989832706747bf8ca7b
unknown <ankitamankar1111@gmail.com>
2022-08-25 18:55:17 +0530

    Configure React app for deployment to GitHub Pages

---
+++ src/App.js
@@ -0,0 +1,22 @@
+
+import React from 'react';
+import './App.css';
+// import Greeting from './greeting';
+// import {Home,Contact} from './componets'
+// import About from './componets';
+// import './style.css'
+
+import Grid from './grid';
+
+
+function App() {
+  
+  return (
+    <div className="App">
+     
+        <Grid />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/componets.jsx
@@ -0,0 +1,19 @@
+ export const Home=()=>{
+    return(
+        <h1>welcom to home component</h1>
+    )
+}
+
+const About=()=>{
+    return(
+        <h1>welcom to about component</h1>
+    )
+}
+
+ export const Contact=()=>{
+    return(
+        <h1>welcom to contact component</h1>
+    )
+}
+
+export default About;
